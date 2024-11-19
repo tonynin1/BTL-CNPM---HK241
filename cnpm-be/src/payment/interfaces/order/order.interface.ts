6e13c27df3fbaaf2140f4a8a12fb4656707f4e94
ThuyVan032004 <thuyvan18032004@gmail.com>
2024-11-19 10:54:47 +0700

    Add prettier endOfLine

---
+++ cnpm-be/src/payment/interfaces/order/order.interface.ts
@@ -0,0 +1,10 @@
+export interface Order {
+    ppoID: number;
+    purchaseTime: Date;
+    customerID: number;
+    ppoStatus: string;
+    pQuantity: number;
+    paperType: string;
+    printerID: number;
+    price: number;
+}
--- cnpm-be/src/payment/payment.controller.ts
+++ cnpm-be/src/payment/payment.controller.ts
@@ -2,6 +2,6 @@ import { Controller } from '@nestjs/common';
 
 @Controller('payment')
 export class PaymentController {
-    
-
+    @post()
+    paymentInfo()
 }
