b00fde0469588dffb0d614c3720f46cbc05f8aff
Le Duc Cuong <leduccuonghcmut@gmail.com>
2024-11-23 12:03:55 +0700

    no message

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,101 +1,71 @@
+'use client';
+
+import { useState } from "react";
 import Image from "next/image";
+import React from 'react';
+import Script from 'next/script';
+import Link from 'next/link';
 
+// export default function Home() {
+//   return (
+//     <main>
+//       <div className="login-container">
+//         <div className="login-box">
+//             <div className="logo-container">
+//                 <Image src="/HCMUT_official_logo.png" alt="Logo" width={60} height={60} className="logo"/>
+//                 <span className="logo-text">HCMUT SPSS</span>
+//             </div>
+//             <h2>Đăng nhập dành cho</h2>
+//             <button className="account-option">
+//                 <i className="icon-admin"></i> 
+//                 <a href="../Login_BTL/index.html">Administrator</a>
+//             </button>
+//             <button className="account-option">
+//                 <i className="icon-admin"></i> 
+//                 <a href="../Login_BTL/index.html">Quản lý hệ thống (SPSO)</a>
+//             </button>
+//             <button className="account-option">
+//                 <i className="icon-user"></i> 
+//                 <a href="../Login_BTL/index.html">Sinh viên Trường đại học Bách khoa</a>
+//             </button>
+//             <div className="register-row">
+//                 <a href="../Sign_up/index.html" className="sign_up">Đăng ký tài khoản mới</a>
+//             </div>
+//         </div>
+//     </div>
+//   </main>
+//   );
+// }
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+      <main>
+        <div className="login-container">
+          <div className="login-box">
+            <div className="logo-container">
+              <Image src="/HCMUT_official_logo.png" alt="Logo" width={60} height={60} className="logo" />
+              <span className="logo-text">HCMUT SPSS</span>
+            </div>
+            <h2>Đăng nhập dành cho</h2>
+            <button className="account-option">
+              <i className="icon-admin"></i>
+              <Link href="/folder_login">Administrator</Link>
+            </button>
+            <button className="account-option">
+              <i className="icon-admin"></i>
+              <Link href="/folder_login">Quản lý hệ thống (SPSO)</Link>
+            </button>
+            <button className="account-option">
+              <i className="icon-user"></i>
+              <Link href="/folder_login">Sinh viên Trường đại học Bách khoa</Link>
+            </button>
+            <div className="register-row">
+              <Link href="/folder_signup" className="sign_up">
+                Đăng ký tài khoản mới
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    );
+  }
+  
