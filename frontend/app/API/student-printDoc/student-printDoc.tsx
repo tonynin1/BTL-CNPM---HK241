7f343420a93cd8b84577e15f596e641c665a48b2
tonynin1 <121789704+tonynin1@users.noreply.github.com>
2024-12-02 02:59:29 +0700

    upload file done

--- frontend/app/API/student-printDoc/student-printDoc.tsx
+++ frontend/app/API/student-printDoc/student-printDoc.tsx
@@ -24,3 +24,34 @@ export async function getAllPrinsAvailable(){
         return null;
     }
 }
+
+export async function uploadFiles(file: File, printerId: number, docQuantity: number, attribute: string, customerId: number){
+    // http://localhost:8080/upload
+    try {
+        const cookies = parseCookies();
+        let accessToken = cookies.accessToken;
+
+        if (!accessToken) {
+            throw new Error("Access token not found.");
+        }
+
+        const formData = new FormData();
+        formData.append('files', file);
+        formData.append('printerId', printerId.toString());
+        formData.append('docQuantity', docQuantity.toString());
+        formData.append('attribute', attribute);
+        formData.append('customerId', customerId.toString());
+
+        const response = await api.post(`upload`, formData, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'multipart/form-data',
+            }
+        })
+
+        return response.data;
+    } catch (error) {
+        console.log("Error uploading files:", error);
+        
+    }
+}
