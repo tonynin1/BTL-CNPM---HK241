6009b3af4da44417fbe214e03a57b1ae40842baf
haitrungng <danisnguyen124@gmail.com>
2024-12-01 14:55:47 +0700

    done student profile

---
+++ frontend/app/student/information/page.tsx
@@ -0,0 +1,165 @@
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import StudentHeader, { StudentHeaderProps } from "@/app/ui/StudentHeader";
+// import { getUserInfo } from "@/app/API/userInfo";
+import { redirect } from "next/navigation";
+import { useUserSession } from "@/app/API/getMe";
+import LoadingPage from "@/app/ui/LoadingPage";
+
+export default function Page() {
+  const { userInfo, loggedIn } = useUserSession();
+
+  // Step 1: Define state to hold form data
+
+  // Handle redirect logic with state
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+  const [editFamilyName, setEditFamilyName] = useState(false);
+
+  const [editFName, setEditFName] = useState(false);
+  const inputLNameRef = useRef<HTMLInputElement>(null);
+  const inputFNameRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      setShouldRedirect(true);
+    } else if (userInfo && userInfo.role === "SPSO") {
+      setShouldRedirect(true);
+    }
+    setFormData({
+      fname: userInfo?.fname,
+      lname: userInfo?.lname,
+    });
+  }, [loggedIn, userInfo]);
+  console.log("USERINFO", userInfo);
+  const [formData, setFormData] = useState({
+    fname: undefined,
+    lname: undefined,
+  });
+  console.log("FORMDATA", formData);
+  // Handle redirect after state update
+  if (shouldRedirect) {
+    if (!loggedIn) redirect("/");
+    if (userInfo?.role === "SPSO") redirect("/spso");
+    return null; // Return null while redirecting
+  }
+
+  if (!userInfo) {
+    return <LoadingPage></LoadingPage>;
+  }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    console.log("Form data:", formData);
+  };
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    console.log(name, value);
+  };
+
+  return (
+    <div className="bg-[#353535] h-[100vh] overflow-hidden">
+      <StudentHeader header={userInfo as StudentHeaderProps} />
+      <div className="flex justify-center p-6 h-[100vh]">
+        <div className="w-1/2 bg-white h-fit rounded-lg p-4 shadow-lg shadow-slate-200">
+          <div className="text-lg text-center font-semibold pb-6">Profile</div>
+          <div>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+              <div className="flex justify-between items-center px-10 gap-10 ">
+                <label htmlFor="lname" className="w-1/3">
+                  Family name
+                </label>
+                <input
+                  type="text"
+                  name="lname"
+                  className="flex-auto text-center p-2"
+                  onChange={handleInputChange}
+                  placeholder="enter your family name"
+                  value={userInfo?.lname}
+                  disabled={!editFamilyName}
+                  autoFocus={editFamilyName}
+                  ref={inputLNameRef}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-edit-3 h-4 w-4 cursor-pointer"
+                  onClick={() => {
+                    setEditFamilyName(true);
+                    inputLNameRef.current.focus();
+                  }}
+                >
+                  <path d="M12 20h9"></path>
+                  <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                </svg>
+              </div>
+              <div className="flex justify-between items-center px-10 gap-10 ">
+                <label htmlFor="fname" className="w-1/3">
+                  First name
+                </label>
+                <input
+                  type="text"
+                  name="fname"
+                  className="flex-auto text-center p-2 "
+                  onChange={handleInputChange}
+                  placeholder="enter your family name"
+                  value={userInfo.fname}
+                  disabled={!editFName}
+                  autoFocus={editFName}
+                  ref={inputFNameRef}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-edit-3 h-4 w-4 cursor-pointer"
+                  onClick={() => {
+                    setEditFName(true);
+                    inputFNameRef.current.focus();
+                  }}
+                >
+                  <path d="M12 20h9"></path>
+                  <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                </svg>
+              </div>
+              <div className="flex justify-between items-center px-10 gap-10 ">
+                <span>Số dư tài khoản</span>
+                <span>{userInfo.accBalance} vnd</span>
+              </div>
+              <div className="flex justify-between items-center px-10 gap-10 ">
+                <span>Remain pages</span>
+                <span>{userInfo?.remainPages} trang</span>
+              </div>
+
+              {(editFName || editFamilyName) && (
+                <button
+                  type="submit"
+                  className="bg-[#353535] text-white rounded-lg p-2"
+                >
+                  Change information
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
